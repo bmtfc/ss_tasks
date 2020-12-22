cb5b64a736cd6a3d49007fd9e7729f88be556bd4
Orest Artekh <orest.artekh.ukr@gmail.com>
2020-12-23 01:32:49 +0200

    created init func

--- task1/main.cpp
+++ task1/main.cpp
@@ -2,9 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
+// init
+// delete
+//add
+//remove
+// number of items
+// first match of string
 
-    std::cout << "Hello world !\n";
+// snprintf() and atio()
+
+void StringListInit(char ***list) {
+    char **t_list = (char **) malloc(1 * sizeof(char *));
+    char buf[128];
+    snprintf(buf, sizeof(buf), "%d", 0);
+    t_list[0] = buf;
+    *list = t_list;
+}
+
+int main() {
+
+    char **list;
+    StringListInit(&list);
+
 
     return 0;
 }
