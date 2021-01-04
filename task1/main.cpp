318477ff1b8e76e1713c15dfed0028de8a89be14
Orest Artekh <orest.artekh.ukr@gmail.com>
2021-01-04 20:23:07 +0200

    add StringRemove function

--- task1/main.cpp
+++ task1/main.cpp
@@ -6,38 +6,38 @@
 
 void StringListInit(char ***list)
 {
-    char **t_list = (char **) malloc(1 * sizeof(char *));
-    t_list[0] = (char *) malloc(MAX_COUNT_OF_DIGITS_IN_INT * sizeof(char));
+    char **temp_list = (char **) malloc(1 * sizeof(char *));
+    temp_list[0] = (char *) malloc(MAX_COUNT_OF_DIGITS_IN_INT * sizeof(char));
     char buf[MAX_COUNT_OF_DIGITS_IN_INT];
     snprintf(buf, sizeof(buf), "%d", 0);
-    t_list[0] = buf;
-    *list = t_list;
+    temp_list[0] = buf;
+    *list = temp_list;
 }
 
 void StringListAdd(char ***list, char *str)
 {
     int list_size = atoi(*list[0]) + 1;
-    char **temp;
-    temp = *list;
+    char **temp_list;
+    temp_list = *list;
     int n = strlen(str);
-    temp = (char **) realloc(temp, (list_size + 1) * sizeof(*temp));
-    temp[list_size] = (char *) malloc((n + 1) * sizeof(char));
+    temp_list = (char **) realloc(temp_list, (list_size + 1) * sizeof(*temp_list));
+    temp_list[list_size] = (char *) malloc((n + 1) * sizeof(char));
     int i;
     for (i = 0; i < n; ++i)
     {
-        temp[list_size][i] = str[i];
+        temp_list[list_size][i] = str[i];
     }
-    temp[list_size][i] = '\0';
+    temp_list[list_size][i] = '\0';
     char buf[MAX_COUNT_OF_DIGITS_IN_INT];
     snprintf(buf, sizeof(buf), "%d", list_size);
-    temp[0] = buf;
-    *list = temp;
+    temp_list[0] = buf;
+    *list = temp_list;
 }
 
 void PrintList(char ***list)
 {
-    int num_of_str = atoi(*list[0]) + 1;
-    for (int i = 1; i < num_of_str; ++i)
+    int list_size = atoi(*list[0]) + 1;
+    for (int i = 1; i < list_size; ++i)
     {
         printf("%s\n", (*list)[i]);
     }
@@ -54,9 +54,9 @@ void StringListDestroy(char ***list)
     *list = nullptr;
 }
 
-int StringListSize(char **list)
+int StringListSize(char ***list)
 {
-    return (atoi(list[0]));
+    return (atoi(*list[0]));
 }
 
 int StringListIndexOf(char **list, char *str)
@@ -72,6 +72,45 @@ int StringListIndexOf(char **list, char *str)
     return (-1);
 }
 
+void StringListRemove(char ***list, char *str)
+{
+    int index_of_str_to_delete = -1;
+
+    char **temp_list, **temp_list_all;
+    temp_list = *list;
+    temp_list_all = *list;
+
+    int list_size = atoi(temp_list[0]) + 1;
+
+    temp_list = (char **) realloc(temp_list, (list_size - 1) * sizeof(char *));
+
+    for (int i = 1; i < list_size; i++)
+    {
+        if (strcmp(temp_list[i], str) == 0)
+        {
+            index_of_str_to_delete = i;
+        }
+    }
+
+    list_size--;
+    int k = 1;
+    for (int i = 1; i < list_size - 1; ++i)
+    {
+        if (i == index_of_str_to_delete)
+        {
+            k++;
+        }
+        strcpy(temp_list[i], temp_list_all[k]);
+        k++;
+    }
+
+
+    char buf[MAX_COUNT_OF_DIGITS_IN_INT];
+    snprintf(buf, sizeof(buf), "%d", list_size - 1);
+    temp_list[0] = buf;
+    *list = temp_list;
+}
+
 int main()
 {
 
@@ -83,8 +122,10 @@ int main()
     StringListAdd(&list, "ghgh");
     StringListAdd(&list, "ghgh");
     PrintList(&list);
-    printf("size of the list : %d\n", StringListSize(list));
-    printf("index of the hhgh in list : %d", StringListIndexOf(list, "hhgh"));
+    printf("size of the list : %d\n", StringListSize(&list));
+    printf("index of the hhgh in list : %d\n", StringListIndexOf(list, "hhgh"));
+    StringListRemove(&list, "hhgh");;
+    PrintList(&list);
     StringListDestroy(&list);
 
     return 0;
